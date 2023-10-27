b30d1896b1d7d1e77de0afa23aa0471126b59d59
RITESHKUMAR7488 <riteshman30@gmail.com>
2023-10-27 08:43:33 +0530

    daily

--- OddEven.java
+++ OddEven.java
@@ -5,7 +5,7 @@ public class OddEven {
             System.out.println("a is even");
         }
         else{
-            System.out.println("a is  1 odd");
+            System.out.println("a is  1 oodd");
         }
     }
     
